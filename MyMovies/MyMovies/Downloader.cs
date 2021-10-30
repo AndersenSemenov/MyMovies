c9ee004a8d4c571a56002349bc8b139f82953b93
Andersen <andersen5447@gmail.com>
2021-10-30 18:06:17 +0300

    start work on parallel pipeline

--- MyMovies/MyMovies/Downloader.cs
+++ MyMovies/MyMovies/Downloader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +9,19 @@ namespace MyMovies
 {
     public static class Downloader
     {
-        public static void Download()
+        public static async Task LoadContentAsync(string path, BlockingCollection<string> output)
         {
-
+            using (FileStream stream = File.OpenRead(path))
+            {
+                var reader = new StreamReader(stream);
+                var firstLine = reader.ReadLine();
+                string line = null;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    output.Add(line);
+                }
+            }
+            output.CompleteAdding();
         }
     }
 }
---
+++ MyMovies/MyMovies/Pipeline.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMovies
+{
+    class Pipeline
+    {
+        public static void ReadAsync()
+        {
+            var bc = new BlockingCollection<string>();
+            var task1 = Downloader.LoadContentAsync(@"D:\data\ml-latest (1)\ml-latest\MovieCodes_IMDB.tsv", bc);
+
+            var dict = new ConcurrentDictionary<string, string>();
+            var task2 = ProcessContentAsync(bc, dict, new char[] { ' ' });
+            task2.Wait();
+
+        }
+
+        public static Task ProcessContentAsync(BlockingCollection<string> input, ConcurrentDictionary<string, string> output, char[] spliters)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                foreach (var line in input.GetConsumingEnumerable())
+                {
+                    string[] words = line.Split(spliters);
+                    if (words[4] == "ru" || words[4] == "en")
+                    {
+                       output.AddOrUpdate(words[0], words[2], ((x, y) => y));
+                    }
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
