0815c779b4fe1c56686c8361ade3addab028573e
Andersen <andersen5447@gmail.com>
2021-12-21 23:03:57 +0300

    version in case evrth gone bad

---
+++ MyMovies/BlazorApp/Parser/ActorDirectorNames.cs
@@ -0,0 +1,36 @@
+﻿using MyMovies.Parser;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyMovies
+{
+    class ActorDirectorNames: DataParser<string, Actor> // key --- personId, value --- personName
+    {
+        public ConcurrentDictionary<string, Director> directorsDict = new ConcurrentDictionary<string, Director>();
+
+        public ActorDirectorNames(): base('\t', @"D:\data\ml-latest (1)\ml-latest\ActorsDirectorsNames_IMDB.txt") { }
+
+        protected override void ParseData()
+        {
+            Regex regex = new Regex("actor|director");
+            foreach (var line in inputFileStrings.GetConsumingEnumerable())
+            {
+                if (regex.IsMatch(line))
+                {
+                    var firstIndex = line.IndexOf(spliter);
+                    var secondIndex = line.IndexOf(spliter, firstIndex + 1);
+
+                    var personId = line.Substring(0, firstIndex);
+                    var personName = line.Substring(firstIndex + 1, secondIndex - firstIndex - 1); 
+
+                    dict.AddOrUpdate(personId, new Actor(personId, personName), ((x, y) => y));
+                    directorsDict.AddOrUpdate(personId, new Director(personId, personName), (x, y) => y);
+                }
+            }
+        }
+    }
+}
---
+++ MyMovies/BlazorApp/Parser/MovieCodes.cs
@@ -0,0 +1,51 @@
+﻿using MyMovies.Parser;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyMovies
+{
+    class MovieCodes : DataParser<string, Movie> // key --- IMDB_Id, value --- movie
+    {
+        public MovieCodes() : base('\t', @"D:\data\ml-latest (1)\ml-latest\MovieCodes_IMDB.tsv") { }
+
+        protected override void ParseData()
+        {
+            Regex regex = new Regex("\ten|ru\t");
+
+            foreach (var line in inputFileStrings.GetConsumingEnumerable())
+            {  
+                if (regex.IsMatch(line))
+                {
+                    var firstIndex = line.IndexOf(spliter);
+                    var secondIndex = line.IndexOf(spliter, firstIndex + 1);
+                    var thirdIndex = line.IndexOf(spliter, secondIndex + 2);
+
+                    var IMDB_Id = line.Substring(0, firstIndex);
+                    var movieName = line.Substring(secondIndex + 1, thirdIndex - secondIndex - 1);
+                    if (Process.tagScores.dict.ContainsKey(IMDB_Id) 
+                        && Process.ratings.dict.ContainsKey(IMDB_Id)
+                        && Process.actorDirectorCodes.dict.ContainsKey(IMDB_Id))
+                    {
+                        var director = Process.actorDirectorCodes.directorDict.ContainsKey(IMDB_Id) 
+                            ? Process.actorDirectorCodes.directorDict[IMDB_Id] : null;
+
+                        dict.AddOrUpdate(IMDB_Id,
+                            new Movie
+                            (IMDB_Id,
+                            movieName,
+                            Process.ratings.dict[IMDB_Id],
+                            Process.actorDirectorCodes.dict[IMDB_Id],
+                            director,
+                            Process.tagScores.dict[IMDB_Id]),
+                            (x, y) => y);
+                    }
+                }
+            }
+        }
+    }
+}
