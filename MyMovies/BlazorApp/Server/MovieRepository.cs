69b985123dab9e516657cf83de7629a406a22fc4
Andersen <andersen5447@gmail.com>
2021-12-23 20:02:40 +0300

    temporary version of application based on WebAssembly

---
+++ MyMovies/BlazorApp/Server/MovieRepository.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Server
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public MovieRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var actor = _context.Actors.Find("nm0000001");
+
+            await _context.Movies.Include(movie => movie.Actors).
+            Where(movie => movie.Actors.Contains(actor)).ToListAsync();
+            return null;
+        }
+            
+    }
+}
---
+++ MyMovies/BlazorApp/Shared/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp.Shared
+{
+    public class Movie
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        //[JsonIgnore]
+        public HashSet<Actor> Actors { get; set; }
+        //[JsonIgnore]
+        public Director Director { get; set; }
+        //[JsonIgnore]
+        public HashSet<Tag> Tags { get; set; }
+        public string TopTen { get; set; }
+
+        public Movie() { }
+
+        public Movie(string id, string name, double rating, HashSet<Actor> actors, Director director, HashSet<Tag> tags)
+        {
+            Id = id;
+            Name = name;
+            Rating = rating;
+            Actors = actors;
+            Director = director;
+            Tags = tags;
+        }
+    }
+}
