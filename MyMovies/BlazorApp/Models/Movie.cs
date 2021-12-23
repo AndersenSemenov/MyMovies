fef8883f82923c4566d37518bf16be5d13e64bb5
Andersen <andersen5447@gmail.com>
2021-12-23 23:32:09 +0300

    working minimal version on server

---
+++ MyMovies/BlazorApp/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Linq;
+
+namespace BlazorApp.Models
+{
+    public class Movie
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public HashSet<Actor> Actors { get; set; }
+        public Director Director { get; set; }
+        public HashSet<Tag> Tags { get; set; }
+        public string TopTen { get; set; }
+
+        public Movie() { }
+
+        public Movie(string id, string name, double rating, HashSet<Actor> actors, Director director, HashSet<Tag> tags)
+        {
+            Id = id;
+            Name = name;
+            Rating = rating;
+            Actors = actors;
+            Director = director;
+            Tags = tags;
+        }
+    }
+}
---
+++ MyMovies/BlazorApp/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class Tag
+    {
+        [Key]
+        public string Value { get; set; }
+        public HashSet<Movie> Movies { get; set; }
+
+        public Tag() { }
+
+        public Tag(string value)
+            => Value = value;
+    }
+}
